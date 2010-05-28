8de2a8475a20e6ee267215bd6ded3acabd64a008
yas <ilyukevich.victor@gmail.com>
2010-05-28 10:59:33 +0300

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_selected_issues_summary do
+  name 'Redmine Selected Issues Summary plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
