f4785eb8677fedad15ea1e104401128df49b21f7
yas <ilyukevich.victor@gmail.com>
2010-06-03 17:53:20 +0300

    add information about author

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require_dependency 'selected_issues_summary_hook'
 
 Redmine::Plugin.register :redmine_selected_issues_summary do
   name 'Redmine Selected Issues Summary plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Yas'
+  description 'View stat for selected tasks on issues page'
   version '0.0.1'
 end
