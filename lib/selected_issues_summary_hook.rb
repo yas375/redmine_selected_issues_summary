8848212a11fcf679a0c0df5ad24fb61054c2a46e
yas <ilyukevich.victor@gmail.com>
2010-06-04 12:06:02 +0300

    change hook

--- lib/selected_issues_summary_hook.rb
+++ lib/selected_issues_summary_hook.rb
@@ -1,5 +1,5 @@
 class SelectedIssuesSummaryHook < Redmine::Hook::ViewListener
-  def view_issues_context_menu_start(context={})
+  def view_issues_sidebar_issues_bottom(context={})
     context[:controller].send(:render_to_string, {
       :partial => "hooks/selected_issues_summary_view_issues_sidebar_issues_bottom",
       :locals => context
